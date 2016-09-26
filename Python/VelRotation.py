1b2a8bf438c6df336ee9687c3882b491759b0a10
iuryt <simoesiury@gmail.com>
2016-09-26 16:34:52 -0300

    Changes on identation

--- Python/VelRotation.py
+++ Python/VelRotation.py
@@ -3,25 +3,25 @@ import numpy as np
 
 def rot_vel(U,V,ang,prop='uv'):
     ''' Rotate velocities along/cross transect
-===========================================================================
+       ===========================================================================
 
-Input :
--> U: W-E velocity
--> V: S-N velocity
--> ang: transect angle [-180,..,180] 
--> prop: return rotated velocity ['u':along; 'v':cross; 'uv':both]
-         Default prop to return is 'uv'
-===========================================================================
+       Input :
+       -> U: W-E velocity
+       -> V: S-N velocity
+       -> ang: transect angle [-180,..,180]
+       -> prop: return rotated velocity ['u':along; 'v':cross; 'uv':both]
+                Default prop to return is 'uv'
+       ===========================================================================
 
-Output:
--> Rotated velocity vectors based on input 'prop'
-===========================================================================
+       Output:
+       -> Rotated velocity vectors based on input 'prop'
+       ===========================================================================
 
-Version 1.0.0
-Hélio Almeida & Dante Napolitano @ Laboratório de Dinâmica Oceânica
-Oceanographic Institute - University of São Paulo
-23-09-2016
-'''
+       Version 1.0.0
+       Hélio Almeida & Dante Napolitano @ Laboratório de Dinâmica Oceânica
+       Oceanographic Institute - University of São Paulo
+       23-09-2016
+    '''
 
     C=np.cos(np.deg2rad(ang))
     S=np.sin(np.deg2rad(ang))
