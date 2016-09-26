a001e2f520ea9a56f19617510ce25bbeb925aea3
Ryukamusa <helio.mgl@gmail.com>
2016-09-26 17:33:13 -0300

    it was not fixed or at least not 100% fixed yet. Not for sure is.

--- Python/VelRotation.py
+++ Python/VelRotation.py
@@ -3,30 +3,33 @@ import numpy as np
 
 def rot_vel(U,V,ang,prop='uv'):
     ''' Rotate velocities along/cross transect
-===========================================================================
-
-Input :
--> U: W-E velocity
--> V: S-N velocity
--> ang: transect angle [-180,..,180] 
--> prop: return rotated velocity ['u':along; 'v':cross; 'uv':both]
-         Default prop to return is 'uv'
-===========================================================================
-
-Output:
--> Rotated velocity vectors based on input 'prop'
-===========================================================================
-
-Version 1.0.0
-Hélio Almeida & Dante Napolitano @ Laboratório de Dinâmica Oceânica
-Oceanographic Institute - University of São Paulo
-23-09-2016
-'''
+    ===========================================================================
+    
+    Input :
+    -> U: W-E velocity
+    -> V: S-N velocity
+    -> ang: transect angle [-180,..,180] 
+    -> prop: return rotated velocity ['u':along; 'v':cross; 'uv':both]
+            Default prop to return is 'uv'
+    ===========================================================================
+    
+    Output:
+    -> Rotated velocity vectors based on input 'prop'
+    ===========================================================================
+    
+    Version 1.0.0
+    Hélio Almeida & Dante Napolitano @ Laboratório de Dinâmica Oceânica
+    Oceanographic Institute - University of São Paulo
+    23-09-2016
+    '''
 
     C=np.cos(np.deg2rad(ang))
     S=np.sin(np.deg2rad(ang))
     Un=U*C+V*S
-    Vn=U*C-V*S
+    Vn=-1.*U*S+V*C
+    if np.abs(ang)>90:
+        Un*=-1.
+        Vn*=-1.
     if prop=='u':
         return Un
     elif prop=='v':
